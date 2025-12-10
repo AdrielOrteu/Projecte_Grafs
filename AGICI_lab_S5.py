b0287604d4ac2ba19b90ce256e18c678a50d9e8a
Maxirenia <maxtrapi@gmail.com>
2025-12-10 12:32:43 +0100

    arxiu s5

---
+++ AGICI_lab_S5.py
@@ -0,0 +1,37 @@
+import sys
+import networkx as nx
+import numpy as np
+
+from typing import List, Tuple
+from io import StringIO
+
+def find_motifs(G: nx.DiGraph) -> List[Tuple[str, str, str]]:
+    """
+    Detect motifs in the directed graph given as input.
+
+    Parameters
+    ----------
+    - param: G : Networkx digraph
+        Graph to analyze
+    - return: list
+        List of detected motifs, each represented as a tuple (X, Y, Z).
+    """
+
+    motifs = []
+    # ------- IMPLEMENT HERE THE BODY OF THE FUNCTION ------- #
+
+    # ----------------- END OF FUNCTION --------------------- #
+    print(len(motifs))
+    return motifs
+
+if __name__ == "__main__":
+
+    # Read all stdin as the GraphML content
+    # E.g.: python3 AGICI_lab_S5.py < toy_1.graphml
+    graphml_content = sys.stdin.read()
+
+    # Load GraphML content a networkx graph
+    G = nx.read_graphml(StringIO(graphml_content))
+
+    # Print number of motifs
+    motifs = find_motifs(G)
